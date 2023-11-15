305d843584f6a936e11856250c7291edd7b7b3f2
Wonjeong Kim <narubrown@users.noreply.github.com>
2023-11-15 23:49:33 +0900

    Initial commit

---
+++ src/test/java/com/example/appcenterstudygitactions/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.appcenterstudygitactions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
